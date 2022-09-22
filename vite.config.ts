945e8665e590653efc6803878e57d817836457c9
834772509 <834772509@qq.com>
2022-09-22 15:02:50 +0800

    perf: element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +15,8 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
-    })
+    }),
+    ElementPlus()
   ],
   resolve: {
     alias: {
