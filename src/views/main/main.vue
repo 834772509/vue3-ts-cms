70dcd5e830225040e17b56225bd3976aae1f8c91
834772509 <834772509@qq.com>
2022-09-24 08:58:22 +0800

    feat: add main page container

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,9 +1,61 @@
 <template>
-  <div>
-    <h2>Main</h2>
+  <div class="main">
+    <el-container class="main-content">
+      <el-aside class="el-aside" width="210px">Aside</el-aside>
+      <el-container class="page">
+        <el-header class="page-header">Header</el-header>
+        <el-main class="page-content">Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.main {
+  position: flex;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.main-content,
+.page {
+  height: 100%;
+}
+
+.page-content {
+  height: calc(100%-48px);
+}
+
+.page-header,
+.page-footer {
+  display: flex;
+  color: #333;
+  text-align: center;
+  align-items: center;
+}
+
+.page-header {
+  height: 48px !important;
+}
+
+.el-aside {
+  overflow-x: hidden;
+  overflow-y: auto;
+  line-height: 200px;
+  text-align: center;
+  background-color: #001529;
+  transition: width 0.3s linear;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+.page-content {
+  color: #333;
+  text-align: center;
+  background-color: #f0f2f5;
+}
+</style>
