6f472a182ee2a9b1739fbf9325ccbcaafc143317
834772509 <834772509@qq.com>
2022-09-28 09:28:51 +0800

    feat: set element-plus i18n to chinese

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import pinia from "./stores";
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 import "normalize.css";
 import "./assets/css/index.less";
 import { setupStores } from "./stores";
@@ -13,5 +15,8 @@ app.use(globalProperties);
 app.use(pinia);
 setupStores();
 app.use(router);
+app.use(ElementPlus, {
+  locale: zhCn
+});
 
 app.mount("#app");
