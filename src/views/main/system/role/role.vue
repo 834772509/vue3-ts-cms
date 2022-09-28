a06ebbf80d4f636c62a9f9f0b948320f38c43e80
834772509 <834772509@qq.com>
2022-09-28 14:40:45 +0800

    feat: add system managle: role page content of use page-search and page-content compenent

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="role">
-    <h2>role</h2>
+    <page-search :searchFormConfig="searchFormConfig"></page-search>
+    <page-content
+      pageName="role"
+      :contentTableConfig="contentTableConfig"
+    ></page-content>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import PageSearch from "@/components/page-search";
+import PageContent from "@/components/page-content";
+import { contentTableConfig } from "./config/content.config";
+import { searchFormConfig } from "./config/search.config";
+</script>
 
 <style lang="less" scoped></style>
