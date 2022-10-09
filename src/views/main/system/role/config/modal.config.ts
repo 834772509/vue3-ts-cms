2ed2e993ee9daeef0c07a31ae3e1f04b84612d59
834772509 <834772509@qq.com>
2022-10-09 10:00:23 +0800

    feat: add role data and edit role info

---
+++ src/views/main/system/role/config/modal.config.ts
@@ -0,0 +1,22 @@
+import type { IForm } from "@/base-ui/form";
+
+export const modalConfig: IForm = {
+  itemStyle: {},
+  colLayout: {
+    span: 24
+  },
+  formItems: [
+    {
+      field: "name",
+      type: "input",
+      label: "角色名",
+      placeholder: "请输入角色名"
+    },
+    {
+      field: "intro",
+      type: "input",
+      label: "角色介绍",
+      placeholder: "请输入角色介绍"
+    }
+  ]
+};
--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -9,18 +9,31 @@
       ref="pageContentRef"
       pageName="role"
       :contentTableConfig="contentTableConfig"
+      @newBtnClick="handleNewData"
+      @editBtnClick="handleEditData"
     ></page-content>
+    <page-modal
+      ref="pageModalRef"
+      :modalConfig="modalConfig"
+      :defaultInfo="defaultInfo"
+      pageName="role"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
 import PageSearch from "@/components/page-search";
 import PageContent from "@/components/page-content";
+import PageModal from "@/components/page-modal";
 import { contentTableConfig } from "./config/content.config";
 import { searchFormConfig } from "./config/search.config";
+import { modalConfig } from "./config/modal.config";
 import { usePageSearch } from "@/hooks/use-page-search";
+import { usePageModal } from "@/hooks/use-page-modal";
 
 const [pageContentRef, handleResetClick, handleSearchClick] = usePageSearch();
+const [pageModalRef, defaultInfo, handleNewData, handleEditData] =
+  usePageModal();
 </script>
 
 <style lang="less" scoped></style>
