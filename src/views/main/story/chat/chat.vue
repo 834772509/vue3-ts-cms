193110c36f280842eb549164a05911ea24f92606
834772509 <834772509@qq.com>
2022-10-14 10:20:14 +0800

    feat: add new story data page content (The project is finish!)

--- src/views/main/story/chat/chat.vue
+++ src/views/main/story/chat/chat.vue
@@ -1,9 +1,64 @@
 <template>
   <div class="chat">
-    <h2>chat</h2>
+    <div class="input">
+      <el-input v-model="title" placeholder="请输入故事标题" />
+      <el-input
+        type="textarea"
+        :rows="20"
+        placeholder="请输入您的故事"
+        v-model="content"
+      />
+    </div>
+    <div class="action">
+      <el-button class="btn-commit" type="primary" @click="commitBtn"
+        >提交</el-button
+      >
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import { createPageData } from "@/service/main/system/system";
+import { ElMessage } from "element-plus";
 
-<style lang="less" scoped></style>
+const title = ref("");
+const content = ref("");
+
+function commitBtn() {
+  createPageData("/story", { title: title.value, content: content.value })
+    .then(() => {
+      title.value = "";
+      content.value = "";
+      ElMessage({
+        message: "提交成功",
+        type: "success"
+      });
+    })
+    .catch(() => {
+      ElMessage({
+        message: "提交失败",
+        type: "error"
+      });
+    });
+}
+</script>
+
+<style lang="less" scoped>
+.chat {
+  padding: 20px;
+  .input {
+    .el-input {
+      padding-bottom: 20px;
+    }
+  }
+
+  .action {
+    text-align: right;
+
+    .btn-commit {
+      margin: 20px 20px 0 10px;
+    }
+  }
+}
+</style>
